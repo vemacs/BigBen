0de0856c446e25f51615977194dfb711b7a3e84d
vemacs <gentoo@outlook.com>
2012-12-08 16:44:19 -0700

    fixed critical bug

--- src/com/nullblock/vemacs/BigBen/BongListener.java
+++ src/com/nullblock/vemacs/BigBen/BongListener.java
@@ -1,7 +1,5 @@
 package com.nullblock.vemacs.BigBen;
 
-import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
 import org.bukkit.event.Listener;
 
 public class BongListener implements Listener{
@@ -10,25 +8,6 @@ public class BongListener implements Listener{
     public BongListener(BigBen BigBen) {
         plugin = BigBen;
         plugin.getServer().getPluginManager().registerEvents(this, plugin);
-        String prefix = "<" + ChatColor.GOLD + "BigBen" + ChatColor.RESET + ">";
-        int nextHour = BongLib.nextHour();
-        int waitTime = BongLib.secondsUntilNextHour();
-        try {
-            Thread.sleep(waitTime * 1000);
-        } catch(InterruptedException ex) {
-            Thread.currentThread().interrupt();
+        new Thread(new BongThread()).start();
         }
-        Bukkit.broadcastMessage(prefix + BongLib.bongText(nextHour));
-        int hour = nextHour;
-        for(;;){
-            try {
-                Thread.sleep(3600000);
-            } catch(InterruptedException ex) {
-                Thread.currentThread().interrupt();
-            }
-            hour++;
-            hour = hour % 12;
-            Bukkit.broadcastMessage(prefix + BongLib.bongText(hour));
-        }
-    }
 }
---
+++ src/com/nullblock/vemacs/BigBen/BongThread.java
@@ -0,0 +1,29 @@
+package com.nullblock.vemacs.BigBen;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+
+public class BongThread implements Runnable {
+    public void run() {
+        String prefix = "<" + ChatColor.GOLD + "BigBen" + ChatColor.RESET + ">";
+        int nextHour = BongLib.nextHour();
+        int waitTime = BongLib.secondsUntilNextHour();
+        try {
+            Thread.sleep(waitTime * 1000);
+        } catch(InterruptedException ex) {
+            Thread.currentThread().interrupt();
+        }
+        Bukkit.broadcastMessage(prefix + BongLib.bongText(nextHour));
+        int hour = nextHour;
+        for(;;){
+            try {
+                Thread.sleep(3600000);
+            } catch(InterruptedException ex) {
+                Thread.currentThread().interrupt();
+            }
+            hour++;
+            hour = hour % 12;
+            Bukkit.broadcastMessage(prefix + BongLib.bongText(hour));
+        }
+    }
+}
