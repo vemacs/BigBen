f40debea92b6d08a438af7929177cd2bc25caf9b
vemacs <gentoo@outlook.com>
2012-12-09 10:00:32 -0700

    OnEnable should load default values now.

--- src/com/nullblock/vemacs/BigBen/BigBen.java
+++ src/com/nullblock/vemacs/BigBen/BigBen.java
@@ -1,5 +1,7 @@
 package com.nullblock.vemacs.BigBen;
 
+import java.util.Arrays;
+
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -7,11 +9,14 @@ public final class BigBen extends JavaPlugin {
     @Override
     public void onEnable() {
     	  FileConfiguration config = this.getConfig();
-    	  config.addDefault("prefix", "<&6BigBen&F>");
+    	  config.addDefault("prefix", "<&6BigBen&r>");
+    	  config.addDefault("prefix", "<&6BigBen&r>");
+    	  String[] responselist = {"Definitely.", "Absolutely not.", "Ask again later.", "Install Gentoo.", "Do you even lift?", "Don't count on it.", "Indeed."};
+    	  this.getConfig().set("responses", Arrays.asList(responselist));
     	  config.options().copyDefaults(true);
     	  saveConfig();
-        new Thread(new BongThread(BongLib.textToColor(BigBen.this.getConfig().getString("prefix")))).start();
-        new ChatListener(this);
+          new Thread(new BongThread(BongLib.textToColor(BigBen.this.getConfig().getString("prefix")))).start();
+          new ChatListener(this);
 
     }
     @Override
